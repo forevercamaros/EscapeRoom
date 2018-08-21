0c5b4b47ea1aaf72b44ad341c4d0a8b50c8fc0cc
forevercamaros <charlie.f.summers@gmail.com>
2018-08-21 14:24:33 -0500

    Added The Video Capabiity

--- app/src/main/java/com/forevercamaros/charlessummers/escaperoom/FullscreenActivity.java
+++ app/src/main/java/com/forevercamaros/charlessummers/escaperoom/FullscreenActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.media.AudioDeviceInfo;
 import android.media.AudioManager;
+import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
@@ -18,18 +19,44 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.assistant.embedded.v1alpha2.SpeechRecognitionResult;
 import com.google.auth.oauth2.UserCredentials;
+import com.pubnub.api.Callback;
+import com.pubnub.api.Pubnub;
+import com.pubnub.api.PubnubError;
+import com.pubnub.api.PubnubException;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnection;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoCapturerAndroid;
+import org.webrtc.VideoRenderer;
+import org.webrtc.VideoRendererGui;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import me.kevingleason.pnwebrtc.PnPeer;
+import me.kevingleason.pnwebrtc.PnRTCClient;
+import me.kevingleason.pnwebrtc.PnSignalingParams;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -47,6 +74,20 @@ public class FullscreenActivity extends AppCompatActivity {
     private static final String PREF_CURRENT_VOLUME = "current_volume";
     private static final int DEFAULT_VOLUME = 100;
     private static final int SAMPLE_RATE = 16000;
+
+    private ListView mChatList;
+    private ChatAdapter mChatAdapter;
+
+    private PnRTCClient pnRTCClient;
+    private VideoSource localVideoSource;
+    private VideoRenderer.Callbacks localRender;
+    private VideoRenderer.Callbacks remoteRender;
+
+    public static final String VIDEO_TRACK_ID = "videoPN";
+    public static final String AUDIO_TRACK_ID = "audioPN";
+    public static final String LOCAL_MEDIA_STREAM_ID = "localStreamPN";
+
+    private GLSurfaceView videoView;
 
     private static final String TAG = FullscreenActivity.class.getSimpleName();
     /**
@@ -72,6 +113,11 @@ public class FullscreenActivity extends AppCompatActivity {
     private static final boolean USE_VOICEHAT_I2S_DAC = Build.DEVICE.equals(BoardDefaults.DEVICE_RPI3);
 
     private EmbeddedAssistant mEmbeddedAssistant;
+    private SharedPreferences mSharedPreferences;
+
+    private String username;
+    private String stdByChannel;
+    private Pubnub mPubNub;
 
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
@@ -90,7 +136,6 @@ public class FullscreenActivity extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
     };
-    private View mControlsView;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
         public void run() {
@@ -99,7 +144,6 @@ public class FullscreenActivity extends AppCompatActivity {
             if (actionBar != null) {
                 actionBar.show();
             }
-            mControlsView.setVisibility(View.VISIBLE);
         }
     };
     private boolean mVisible;
@@ -130,26 +174,29 @@ public class FullscreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_fullscreen);
 
-        SharedPreferences sp = getSharedPreferences(Constants.SHARED_PREFS,MODE_PRIVATE);
-        SharedPreferences.Editor edit = sp.edit();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        edit.putBoolean(Constants.DISPLAY_ON, false);
+
+        mSharedPreferences = getSharedPreferences(Constants.SHARED_PREFS,MODE_PRIVATE);
+        SharedPreferences.Editor edit = mSharedPreferences.edit();
+
+        edit.putBoolean(Constants.DISPLAY_ON, true);
         edit.apply();
 
         boolean displayOn  = true;
-        displayOn = sp.getBoolean(Constants.DISPLAY_ON,true);
+        displayOn = mSharedPreferences.getBoolean(Constants.DISPLAY_ON,true);
 
 
 
-        FrameLayout fl = (FrameLayout) findViewById(R.id.frame_layout);
+        RelativeLayout fl = (RelativeLayout) findViewById(R.id.relative_layout);
         if (!displayOn){
             fl.setVisibility(View.INVISIBLE);
         }
 
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
+
 
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/my_imaginary_friend.ttf");
 
@@ -161,14 +208,13 @@ public class FullscreenActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 toggle();
+                sendMessage();
             }
         });
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-
         mMainHandler = new Handler(getMainLooper());
 
         UserCredentials userCredentials = null;
@@ -284,6 +330,190 @@ public class FullscreenActivity extends AppCompatActivity {
                 })
                 .build();
         mEmbeddedAssistant.connect();
+
+        this.username     = "ESCAPE_ROOM";
+        this.stdByChannel = this.username + Constants.STDBY_SUFFIX;
+
+        initPubNub();
+
+        // To create our VideoRenderer, we can use the included VideoRendererGui for simplicity
+        // First we need to set the GLSurfaceView that it should render to
+        this.videoView = (GLSurfaceView) findViewById(R.id.gl_surface);
+
+        // Then we set that view, and pass a Runnable to run once the surface is ready
+        VideoRendererGui.setView(videoView, null);
+    }
+
+    /**
+     * Subscribe to standby channel so that it doesn't interfere with the WebRTC Signaling.
+     */
+    public void initPubNub(){
+        this.mPubNub  = new Pubnub(Constants.PUB_KEY, Constants.SUB_KEY);
+        this.mPubNub.setUUID(this.username);
+        subscribeStdBy();
+    }
+
+    /**
+     * Subscribe to standby channel
+     */
+    private void subscribeStdBy(){
+        try {
+            this.mPubNub.subscribe(this.stdByChannel, new Callback() {
+                @Override
+                public void successCallback(String channel, Object message) {
+                    Log.d("MA-iPN", "MESSAGE: " + message.toString());
+                    if (!(message instanceof JSONObject)) return; // Ignore if not JSONObject
+                    JSONObject jsonMsg = (JSONObject) message;
+                    try {
+                        if (!jsonMsg.has(Constants.JSON_CALL_USER)) return;     //Ignore Signaling messages.
+                        String user = jsonMsg.getString(Constants.JSON_CALL_USER);
+                        dispatchIncomingCall(user);
+                    } catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void connectCallback(String channel, Object message) {
+                    Log.d("MA-iPN", "CONNECTED: " + message.toString());
+                    setUserStatus(Constants.STATUS_AVAILABLE);
+                }
+
+                @Override
+                public void errorCallback(String channel, PubnubError error) {
+                    Log.d("MA-iPN","ERROR: " + error.toString());
+                }
+            });
+        } catch (PubnubException e){
+            Log.d("HERE","HEREEEE");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Handle incoming calls.
+     * @param userId
+     */
+    private void dispatchIncomingCall(String userId){
+        /*Intent intent = new Intent(MainActivity.this, IncomingCallActivity.class);
+        intent.putExtra(Constants.USER_NAME, username);
+        intent.putExtra(Constants.CALL_USER, userId);
+        startActivity(intent);*/
+        //TODO Answer Call Automatically
+        this.mChatList     = findViewById(R.id.chat_list);
+
+        List<ChatMessage> ll = new LinkedList<ChatMessage>();
+        mChatAdapter = new ChatAdapter(this, ll);
+        FullscreenActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mChatList.setAdapter(mChatAdapter);
+            }
+        });
+
+        PeerConnectionFactory.initializeAndroidGlobals(
+                this,  // Context
+                true,  // Audio Enabled
+                true,  // Video Enabled
+                true,  // Hardware Acceleration Enabled
+                null); // Render EGL Context
+
+        PeerConnectionFactory pcFactory = new PeerConnectionFactory();
+        this.pnRTCClient = new PnRTCClient(Constants.PUB_KEY, Constants.SUB_KEY, this.username);
+        List<PeerConnection.IceServer> servers = getXirSysIceServers();
+        if (!servers.isEmpty()){
+            this.pnRTCClient.setSignalParams(new PnSignalingParams());
+        }
+
+        // Returns the number of cams & front/back face device name
+        int camNumber = VideoCapturerAndroid.getDeviceCount();
+        String frontFacingCam = VideoCapturerAndroid.getNameOfFrontFacingDevice();
+        String backFacingCam = VideoCapturerAndroid.getNameOfBackFacingDevice();
+
+        // Creates a VideoCapturerAndroid instance for the device name
+        VideoCapturer capturer = VideoCapturerAndroid.create(frontFacingCam);
+
+        // First create a Video Source, then we can make a Video Track
+        localVideoSource = pcFactory.createVideoSource(capturer, this.pnRTCClient.videoConstraints());
+        VideoTrack localVideoTrack = pcFactory.createVideoTrack(VIDEO_TRACK_ID, localVideoSource);
+
+        // First we create an AudioSource then we can create our AudioTrack
+        AudioSource audioSource = pcFactory.createAudioSource(this.pnRTCClient.audioConstraints());
+        AudioTrack localAudioTrack = pcFactory.createAudioTrack(AUDIO_TRACK_ID, audioSource);
+
+
+        // Now that VideoRendererGui is ready, we can get our VideoRenderer.
+        // IN THIS ORDER. Effects which is on top or bottom
+        remoteRender = VideoRendererGui.create(0, 0, 100, 100, VideoRendererGui.ScalingType.SCALE_ASPECT_FILL, false);
+        localRender = VideoRendererGui.create(0, 0, 100, 100, VideoRendererGui.ScalingType.SCALE_ASPECT_FILL, true);
+
+        // We start out with an empty MediaStream object, created with help from our PeerConnectionFactory
+        //  Note that LOCAL_MEDIA_STREAM_ID can be any string
+        MediaStream mediaStream = pcFactory.createLocalMediaStream(LOCAL_MEDIA_STREAM_ID);
+
+        // Now we can add our tracks.
+        mediaStream.addTrack(localVideoTrack);
+        mediaStream.addTrack(localAudioTrack);
+
+        // First attach the RTC Listener so that callback events will be triggered
+        this.pnRTCClient.attachRTCListener(new DemoRTCListener());
+
+        // Then attach your local media stream to the PnRTCClient.
+        //  This will trigger the onLocalStream callback.
+        this.pnRTCClient.attachLocalMediaStream(mediaStream);
+
+        // Listen on a channel. This is your "phone number," also set the max chat users.
+        this.pnRTCClient.listenOn(this.username);
+        this.pnRTCClient.setMaxConnections(1);
+
+        connectToUser(userId, false);
+    }
+
+    public void sendMessage() {
+        String message = "Test";
+        if (message.equals("")) return; // Return if empty
+        ChatMessage chatMsg = new ChatMessage(this.username, message, System.currentTimeMillis());
+        mChatAdapter.addMessage(chatMsg);
+        JSONObject messageJSON = new JSONObject();
+        try {
+            //messageJSON.put(Constants.JSON_MSG_UUID, chatMsg.getSender());
+            messageJSON.put(Constants.JSON_MSG_UUID, chatMsg.getSender());
+            messageJSON.put(Constants.JSON_MSG, chatMsg.getMessage());
+            messageJSON.put(Constants.JSON_TIME, chatMsg.getTimeStamp());
+            this.pnRTCClient.transmitAll(messageJSON);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public void connectToUser(String user, boolean dialed) {
+        this.pnRTCClient.connect(user, dialed);
+    }
+
+    public List<PeerConnection.IceServer> getXirSysIceServers(){
+        List<PeerConnection.IceServer> servers = new ArrayList<PeerConnection.IceServer>();
+        try {
+            servers = new XirSysRequest().execute().get();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }catch (ExecutionException e){
+            e.printStackTrace();
+        }
+        return servers;
+    }
+
+    private void setUserStatus(String status){
+        try {
+            JSONObject state = new JSONObject();
+            state.put(Constants.JSON_STATUS, status);
+            this.mPubNub.setState(this.stdByChannel, this.username, state, new Callback() {
+                @Override
+                public void successCallback(String channel, Object message) {
+                    Log.d("MA-sUS","State Set: " + message.toString());
+                }
+            });
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -310,7 +540,6 @@ public class FullscreenActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
-        mControlsView.setVisibility(View.GONE);
         mVisible = false;
 
         // Schedule a runnable to remove the status and navigation bar after a delay
@@ -339,7 +568,7 @@ public class FullscreenActivity extends AppCompatActivity {
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
-    public void RunCommand(android.view.View View) {
+    /*public void RunCommand(android.view.View View) {
         TextView fullscreen_content = (TextView)findViewById(R.id.fullscreen_content);
         if (fullscreen_content.getText() == "Now You DIE!!!!" || fullscreen_content.getText() == getString(R.string.dummy_content)){
             mEmbeddedAssistant.startConversation("turn on Family Room Lamp");
@@ -361,7 +590,7 @@ public class FullscreenActivity extends AppCompatActivity {
             }
         }.start();
 
-    }
+    }*/
 
     private AudioDeviceInfo findAudioDevice(int deviceFlag, int deviceType) {
         AudioManager manager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
@@ -372,6 +601,77 @@ public class FullscreenActivity extends AppCompatActivity {
             }
         }
         return null;
+    }
+
+    /**
+     * LogRTCListener is used for debugging purposes, it prints all RTC messages.
+     * DemoRTC is just a Log Listener with the added functionality to append screens.
+     */
+    private class DemoRTCListener extends LogRTCListener {
+        @Override
+        public void onLocalStream(final MediaStream localStream) {
+            super.onLocalStream(localStream); // Will log values
+            FullscreenActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(localStream.videoTracks.size()==0) return;
+                    localStream.videoTracks.get(0).addRenderer(new VideoRenderer(localRender));
+                }
+            });
+        }
+
+        @Override
+        public void onAddRemoteStream(final MediaStream remoteStream, final PnPeer peer) {
+            super.onAddRemoteStream(remoteStream, peer); // Will log values
+            FullscreenActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if(remoteStream.audioTracks.size()==0 || remoteStream.videoTracks.size()==0) return;
+                        remoteStream.videoTracks.get(0).addRenderer(new VideoRenderer(remoteRender));
+                        VideoRendererGui.update(remoteRender, 0, 0, 100, 100, VideoRendererGui.ScalingType.SCALE_ASPECT_FILL, false);
+                        VideoRendererGui.update(localRender, 72, 65, 25, 25, VideoRendererGui.ScalingType.SCALE_ASPECT_FIT, true);
+                    }
+                    catch (Exception e){ e.printStackTrace(); }
+                }
+            });
+        }
+
+        @Override
+        public void onMessage(PnPeer peer, Object message) {
+            super.onMessage(peer, message);  // Will log values
+            if (!(message instanceof JSONObject)) return; //Ignore if not JSONObject
+            JSONObject jsonMsg = (JSONObject) message;
+            try {
+                String uuid = jsonMsg.getString(Constants.JSON_MSG_UUID);
+                String msg  = jsonMsg.getString(Constants.JSON_MSG);
+                long   time = jsonMsg.getLong(Constants.JSON_TIME);
+                if (uuid.equals("time")){
+                    final String locMsg = msg;
+                    FullscreenActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mContentView.setText(locMsg);
+                        }
+                    });
+                }else{
+                    final ChatMessage chatMsg = new ChatMessage(uuid, msg, time);
+                    FullscreenActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mChatAdapter.addMessage(chatMsg);
+                        }
+                    });
+                }
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onPeerConnectionClosed(PnPeer peer) {
+            super.onPeerConnectionClosed(peer);
+        }
     }
 
 }
