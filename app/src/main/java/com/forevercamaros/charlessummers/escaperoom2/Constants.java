8dafc3a39b34f1f1c78431140724eae9c1e76cdc
forevercamaros <charlie.f.summers@gmail.com>
2018-09-13 22:49:15 -0500

    Standard Checkin

---
+++ app/src/main/java/com/forevercamaros/charlessummers/escaperoom2/Constants.java
@@ -0,0 +1,18 @@
+package com.forevercamaros.charlessummers.escaperoom2;
+
+/**
+ * Created by GleasonK on 7/30/15.
+ */
+public class Constants {
+    public static final String SHARED_PREFS = "com.forevercamaros.charlessummers.escaperoom2.SHARED_PREFS";
+    public static final String DISPLAY_ON   = "com.forevercamaros.charlessummers.escaperoom2.SHARED_PREFS.DISPLAY_ON";
+    public static final String STDBY_SUFFIX = "-stdby";
+
+    public static final String PUB_KEY = "pub-c-c09ff1a1-51d2-4ef6-bd8d-560d618e80c4";
+    public static final String SUB_KEY = "sub-c-068c1e3c-a3ee-11e8-96b6-ea0414d4b606";
+
+    public static final String JSON_CALL_USER = "call_user";
+    public static final String JSON_STATUS    = "status";
+
+    public static final String STATUS_AVAILABLE = "Available";
+}
